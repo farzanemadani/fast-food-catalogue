d91233f3f1429c63d526afe9e6c579d16f180078
madani93 <madani@citynet.ir>
2023-07-12 23:06:39 +0330

    update category List

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,17 +1,40 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../axios";
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     const fetchCategory = async () => {
-        const response = await axios.get('/FoodCategory/categories');
-        console.log(response.data);
-    }
+      const response = await axios.get("/FoodCategory/categories");
+      setCategories(response.data);
+    };
 
     fetchCategory();
-    
   }, []);
 
-  return <div>category List</div>;
+  return (
+    <nav className="container mt-n5">
+      <div
+        className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
+        style={{ height: "80px" }}
+      >
+        <ul className="nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              همه فست فود ها
+            </a>
+          </li>
+          {categories.map((category) => (
+            <li className="nav-item" key={category.id}>
+              <a href="#" className="nav-link">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 export default CategoryList;

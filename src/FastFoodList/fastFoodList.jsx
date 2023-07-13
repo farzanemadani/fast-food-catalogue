4d73b211b26d14774a0a5aea21aa84764a7b1c27
madani93 <madani@citynet.ir>
2023-07-14 01:34:31 +0330

    add fastFood Item

--- src/FastFoodList/fastFoodList.jsx
+++ src/FastFoodList/fastFoodList.jsx
@@ -1,9 +1,15 @@
 import React from "react"
-
+import FastFoodItem from "../FastFoodItem/fastFoodItem";
 const FastFoodList = ({fastFoodItems}) => {
   return (
-    <div>
-      fastFoodItems
+    <div className="row">
+      {fastFoodItems.map( fastfood => {
+        return (
+          <div className="col-md-4 col-sm-6 mb-grid-gutter" key={fastfood.id}>
+            <FastFoodItem {...fastfood} />
+          </div>
+        )
+      })}
     </div>
   )
 };
